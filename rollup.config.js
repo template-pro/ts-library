c8c0857c8c0aa625c011ed5c6fc17c79c314ba8a
Wuxh <wxh1220@gmail.com>
2021-12-21 10:46:16 +0800

    chore: 使用 rollup-plugin-dts 替换 api-extractor

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'rollup'
 import esbuild from 'rollup-plugin-esbuild'
 import buble from '@rollup/plugin-buble'
 import { terser } from 'rollup-plugin-terser'
+import dts from 'rollup-plugin-dts'
 
 import pkg from './package.json'
 
@@ -12,34 +13,46 @@ const banner = `/*!
 * (c) ${new Date().getFullYear()} ${pkg.author.name}<${pkg.author.email}>
 */`
 
-export default defineConfig({
-  input: resolve(__dirname, 'src/index.ts'),
-  output: [
-    {
-      file: pkg.main,
-      format: 'umd',
-      name: moduleName,
-      exports: 'named',
-      sourcemap: 'inline',
-      banner,
-    },
-    {
-      file: pkg.main.replace('.js', '.min.js'),
-      format: 'umd',
-      name: moduleName,
-      exports: 'named',
-      // sourcemap: "inline",
-      banner,
-      plugins: [terser()],
-    },
-    {
-      file: pkg.module,
-      format: 'esm',
-      banner,
-    },
-  ],
-  plugins: [
-    esbuild(),
-    buble(),
-  ],
-})
+export default [].concat(
+  defineConfig({
+    input: resolve(__dirname, 'src/index.ts'),
+    output: [
+      {
+        file: pkg.main,
+        format: 'umd',
+        name: moduleName,
+        exports: 'named',
+        sourcemap: 'inline',
+        banner,
+      },
+      {
+        file: pkg.main.replace('.js', '.min.js'),
+        format: 'umd',
+        name: moduleName,
+        exports: 'named',
+        // sourcemap: "inline",
+        banner,
+        plugins: [terser()],
+      },
+      {
+        file: pkg.module,
+        format: 'esm',
+        banner,
+      },
+    ],
+    plugins: [
+      esbuild(),
+      buble(),
+    ],
+  }),
+  defineConfig({
+    input: resolve(__dirname, 'src/index.ts'),
+    output: [
+      {
+        file: pkg.types,
+        format: 'es',
+      },
+    ],
+    plugins: [dts()],
+  }),
+)
